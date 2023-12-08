54ddd07271bf1ad6e2cf61a50efe6218f0b6c889
SeliyandaMafeking <148527992+SeliyandaMafeking@users.noreply.github.com>
2023-12-08 16:05:02 +0200

    Added code for Assignment 3

---
+++ Assignment3Sln/Assignment3/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree bigTree = new Tree(40, 20, 30, true);
+
+            Console.WriteLine("Number of leaves: " + bigTree.NumLeaves);
+            Console.WriteLine("Number of branches: " + bigTree.NumBraches);
+            Console.WriteLine("Number of roots: " + bigTree.NumRoots);
+            Console.WriteLine("Tree colour: " + bigTree.IsTreeGreen);
+        }
+    }
+}
---
+++ Assignment3Sln/Assignment3/Tree.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    internal class Tree
+    {
+        public int NumLeaves;
+         public int NumBraches;
+        public int NumRoots;
+         public bool IsTreeGreen;
+
+        public Tree (int numLeaves, int numBranches, int numRoots, bool isTreeGreen)
+        {
+            NumLeaves = numLeaves;
+            NumBraches = numBranches;
+            NumRoots = numRoots;
+            IsTreeGreen = isTreeGreen;
+
+        }
+
+        public int CountLeaves()
+        {
+            return NumLeaves;
+        }
+        public int CountBraches()
+        {
+            return NumBraches;
+        }
+        public int CountRoots()
+        {
+            return NumRoots;
+        }
+        public bool DetermineColour()
+        {
+            return IsTreeGreen;
+        }
+
+    }
+}
